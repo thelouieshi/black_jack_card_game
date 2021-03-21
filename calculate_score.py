93cd891bf322faa76fb48247634cf1b92b08aed3
Louie Shi <61924766+thlouieshi@users.noreply.github.com>
2021-03-21 16:07:16 -0600

    Update calculate_score.py

--- calculate_score.py
+++ calculate_score.py
@@ -1,3 +1,5 @@
+# Black Jack is 21 points, assigned with return of 0
+
 class Calculate_Score:
 
     def calculate(self, cards):
@@ -5,7 +7,7 @@ class Calculate_Score:
         if total_score > 21 and 11 in cards:
             cards.remove(11)
             cards.append(1)
-            return sum(cards)
+            return sum(cards) # note that here the return is sum(cards) and not variable total_points. It's because total_points will return a sum before replacing 11 with 1
         elif total_score == 21:
             return 0
         else:
